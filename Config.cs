01e55f03545705059cccf22968b48b796b00ec00
Lewis Mercer <lewismercer@hotmail.co.uk>
2021-11-30 02:33:28 +0000

    First commit

---
+++ Config.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+
+namespace Autowho
+{
+    static class Config
+    {
+
+        private static IDictionary<string, string> config;
+
+        private static void CreateConfig()
+        {
+            config = new Dictionary<string, string>();
+
+            string configFile = @"Autowho.txt";
+
+            string configUrl = Path.GetFileName(configFile);
+            if (!File.Exists(configUrl))
+            {
+                File.Copy(configFile, configUrl);
+            }
+
+            var fileStream = File.Open(configUrl, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+            using (var stream = fileStream)
+            using (var reader = new StreamReader(stream, Encoding.UTF8))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] values = line.Split(": ");
+                    if (!config.ContainsKey(values[0]))
+                    {
+                        config.Add(values[0], values[1]);
+                    }
+                }
+            }
+            fileStream.Close();
+        }
+
+        /*
+        They key must be saved as scan code. I can't find a way to convert string to scan code and it doesnt matter since we can register the key event 
+        when we make the GUI and by default, gives you the scan code. By default it is 75 which is 'k'. If you set it to default/27 (Escape) then it will do the manual /who
+        */
+
+
+        public static byte GetAutoWhoKey()
+        {
+            return Byte.Parse(config["key"]);
+        }
+
+        public static string getLogLocation()
+        {
+            if(config["log"] == "default")
+            {
+                return Directory.GetCurrentDirectory() + "/logs/latest.log";
+            }
+
+            return config["log"];
+        }
+
+        public static void Create()
+        {
+            CreateConfig();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Autowho
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Config.Create();
+
+            int log = StartLog();
+
+            [DllImport("user32.dll")]
+            static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+
+            Console.WriteLine("Running in background...");
+
+
+            while (true)
+            {
+                var fileStream = File.Open(Config.getLogLocation(), FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                List<String> logFile = new List<string>();
+                using (var stream = fileStream)
+                using (var reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        logFile.Add(line);
+                    }
+                }
+                logFile.RemoveRange(0, log);
+                for (int i = 0; i < logFile.Count; i++)
+                {
+                    try
+                    {
+                        string o = logFile[i];
+                        string[] words = o.Split();
+                        if ((o.Substring(39, o.Length - 39) == "        "))
+                        {
+                            [DllImport("User32.dll")]
+                            static extern int SetForegroundWindow(IntPtr point);
+
+                            Process p = Process.GetProcessesByName("javaw")[0];
+                            if (p != null)
+                            {
+                                IntPtr h = p.MainWindowHandle;
+                                SetForegroundWindow(h);
+
+                                Thread.Sleep(1500);
+
+                                const uint KEYUP = 0x0002;
+                                byte key = Config.GetAutoWhoKey();
+
+                                keybd_event(key, 0, 0, 0);
+                                Thread.Sleep(500);
+                                keybd_event(key, 0, KEYUP, 0);
+                            }
+                        }
+                    }
+                    catch (Exception t)
+                    {
+                        Debug.WriteLine(t);
+                    }
+                }
+                log += logFile.Count;
+            }
+        }
+
+        public static int StartLog()
+        {
+            var lineCount = 0;
+            var fileStream = File.Open(Config.getLogLocation(), FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            using (var stream = fileStream)
+            using (var reader = new StreamReader(stream, Encoding.UTF8))
+            {
+                while (reader.ReadLine() != null)
+                {
+                    lineCount++;
+                }
+            }
+            return lineCount;
+        }
+    }
+}
